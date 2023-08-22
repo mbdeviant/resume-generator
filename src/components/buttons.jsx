9911f2f61536183e38ba6bb6cfd738bac2425fa3
deviant <mb.deviant@gmail.com>
2023-08-22 04:29:51 +0300

    create buttons component

---
+++ src/components/buttons.jsx
@@ -0,0 +1,9 @@
+function Buttons() {
+  return (
+    <div className="btnContainer">
+      <button type="submit">Save</button>
+      <button type="button">Clear</button>
+    </div>
+  );
+}
+export default Buttons;
