7f41ff4a55ced9695295b1ee5994f24a38543b9e
deviant <mb.deviant@gmail.com>
2023-08-22 04:30:47 +0300

    create experience info form

--- src/components/CVForm/Experience.jsx
+++ src/components/CVForm/Experience.jsx
@@ -1,7 +1,24 @@
+import Buttons from "../buttons";
+
 function Experience() {
   return (
     <section className="experience">
       <h2>Experience</h2>
+      <form className="experienceForm">
+        <label htmlFor="position">Position</label>
+        <input type="text" id="position" />
+        <label htmlFor="company">Company</label>
+        <input type="text" id="company" />
+        <label htmlFor="startDate">Start Date</label>
+        <input type="date" id="startDate" />
+        <label htmlFor="endDate">End Date</label>
+        <input type="date" id="endDate" />
+        <label htmlFor="description">Description</label>
+        <input type="text" id="description" />
+        <Buttons />
+      </form>
     </section>
   );
 }
+
+export default Experience;
