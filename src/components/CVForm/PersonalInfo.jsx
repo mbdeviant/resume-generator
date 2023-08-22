90f556c301b4069e0fb05f06845041833e5a958b
deviant <mb.deviant@gmail.com>
2023-08-22 04:30:18 +0300

    wrap input elements with form

--- src/components/CVForm/PersonalInfo.jsx
+++ src/components/CVForm/PersonalInfo.jsx
@@ -1,22 +1,25 @@
+import Buttons from "../buttons";
+
 function PersonalInfo() {
   return (
     <>
       <section className="personalInfo">
         <h2>Personal Info</h2>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name"></input>
-        <label htmlFor="surname">Surname</label>
-        <input type="text" id="surname" />
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" />
-        <label htmlFor="adress">Adress</label>
-        <input type="text" id="adress" />
-        <label htmlFor="number">Phone Number</label>
-        <input type="tel" id="number" />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="about">About</label>
-        <input type="text" id="about" />
+        <form className="infoForm">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" required></input>
+          <label htmlFor="surname">Surname</label>
+          <input type="text" id="surname" required />
+          <label htmlFor="adress">Adress</label>
+          <input type="text" id="adress" required />
+          <label htmlFor="number">Phone Number</label>
+          <input type="tel" id="number" required />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" required />
+          <label htmlFor="about">About</label>
+          <input type="text" id="about" />
+          <Buttons />
+        </form>
       </section>
     </>
   );
