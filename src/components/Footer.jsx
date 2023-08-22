a24ca3f1c1523a6ad0976ff633032f23b9317b6c
deviant <mb.deviant@gmail.com>
2023-08-22 19:03:04 +0300

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p>deviant</p>
+        <a href="https://github.com/mbdeviant">
+          <img src="/github-mark-white.png" alt="github logo" />
+        </a>
+      </footer>
+    </>
+  );
+}
