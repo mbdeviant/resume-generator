d2b329ee251092fd11d751ce17b473b5ef8881cb
deviant <mb.deviant@gmail.com>
2023-08-22 04:30:31 +0300

    create educational info form

---
+++ src/components/CVForm/Education.jsx
@@ -0,0 +1,22 @@
+import Buttons from "../buttons";
+
+function Education() {
+  return (
+    <section className="education">
+      <h2>Education</h2>
+      <form className="educationForm">
+        <label htmlFor="department">Department</label>
+        <input type="text" id="department" required />
+        <label htmlFor="university">University</label>
+        <input type="text" id="university" required />
+        <label htmlFor="startDate">Start Date</label>
+        <input type="date" id="startDate" required />
+        <label htmlFor="graduation">Graduation</label>
+        <input type="date" id="graduation" required />
+        <Buttons />
+      </form>
+    </section>
+  );
+}
+
+export default Education;
