781648980de00c2290d7a7c451a71d640f7df2eb
deviant <mb.deviant@gmail.com>
2023-08-22 04:29:31 +0300

    render personal info form components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Education from "./components/CVForm/Education";
+import Experience from "./components/CVForm/Experience";
 import PersonalInfo from "./components/CVForm/PersonalInfo";
 
 function App() {
   return (
     <div className="app">
       <PersonalInfo />
+      <Experience />
+      <Education />
     </div>
   );
 }
