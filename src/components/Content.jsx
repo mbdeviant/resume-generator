54f28bdae1dd39c04237fdc80e78c31e43f86c5a
deviant <mb.deviant@gmail.com>
2023-08-22 17:47:58 +0300

    render info form components in content component

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Education from "./CVForm/Education";
+import Experience from "./CVForm/Experience";
+import PersonalInfo from "./CVForm/PersonalInfo";
+
+export default function Content() {
+  return (
+    <>
+      <PersonalInfo />
+      <Experience />
+      <Education />
+    </>
+  );
+}
