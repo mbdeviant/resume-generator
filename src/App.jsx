82054a179621eb66322dba57bd427be2f8e91da5
deviant <mb.deviant@gmail.com>
2023-08-25 21:44:02 +0300

    modify rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Content from "./components/Content";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <Header />
       <Content />
-      <Footer />
     </div>
   );
 }
