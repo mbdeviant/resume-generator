c3fa6ff4a76511dfb81f2b8c7a535fd37a3303e2
deviant <mb.deviant@gmail.com>
2023-08-26 21:36:36 +0300

    remove classname for app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="app">
+    <div>
       <Header />
       <div className="main-container">
         <Content />
