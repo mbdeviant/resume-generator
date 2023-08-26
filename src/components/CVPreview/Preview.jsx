9e50f5b55fe8d0cb218e87a5b0475135729d2a7d
deviant <mb.deviant@gmail.com>
2023-08-26 23:58:43 +0300

    add props to update preview display on user input

--- src/components/CVPreview/Preview.jsx
+++ src/components/CVPreview/Preview.jsx
@@ -2,10 +2,24 @@ import EducationPreview from "./EducationPreview";
 import ExperiencePreview from "./ExperiencePreview";
 import PersonalPreview from "./PersonalPreview";
 
-export default function Preview() {
+export default function Preview({
+  name,
+  surname,
+  adress,
+  phone,
+  email,
+  about,
+}) {
   return (
     <section className="preview" aria-label="cv preview">
-      <PersonalPreview />
+      <PersonalPreview
+        name={name}
+        surname={surname}
+        adress={adress}
+        phone={phone}
+        email={email}
+        about={about}
+      />
       <EducationPreview />
       <ExperiencePreview />
     </section>
