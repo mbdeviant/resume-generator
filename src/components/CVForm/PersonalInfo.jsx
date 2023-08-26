531222231b4f026269ab56333ad1d5fb34a7bdc8
deviant <mb.deviant@gmail.com>
2023-08-26 23:08:09 +0300

    create props for personal info inputs

--- src/components/CVForm/PersonalInfo.jsx
+++ src/components/CVForm/PersonalInfo.jsx
@@ -1,23 +1,30 @@
 import Buttons from "../buttons";
 
-export default function PersonalInfo() {
+export default function PersonalInfo({
+  onName,
+  onSurname,
+  onAdress,
+  onPhone,
+  onEmail,
+  onAbout,
+}) {
   return (
     <>
       <section className="personalInfo">
         <h2>Personal Info</h2>
         <form className="infoForm">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" required></input>
+          <input type="text" id="name" onChange={onName} required></input>
           <label htmlFor="surname">Surname</label>
-          <input type="text" id="surname" required />
+          <input type="text" id="surname" onChange={onSurname} required />
           <label htmlFor="adress">Adress</label>
-          <input type="text" id="adress" required />
+          <input type="text" id="adress" onChange={onAdress} required />
           <label htmlFor="number">Phone Number</label>
-          <input type="tel" id="number" required />
+          <input type="tel" id="number" onChange={onPhone} required />
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" onChange={onEmail} required />
           <label htmlFor="about">About</label>
-          <input type="text" id="about" />
+          <input type="text" id="about" onChange={onAbout} />
           <Buttons />
         </form>
       </section>
