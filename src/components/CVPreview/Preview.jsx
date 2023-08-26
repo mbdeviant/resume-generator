8e80705b6ee0f4a5be28882fdc9fbcac9e6267dc
deviant <mb.deviant@gmail.com>
2023-08-26 22:00:00 +0300

    render preview components in one component

--- src/components/CVPreview/Preview.jsx
+++ src/components/CVPreview/Preview.jsx
@@ -1,3 +1,13 @@
+import EducationPreview from "./EducationPreview";
+import ExperiencePreview from "./ExperiencePreview";
+import PersonalPreview from "./PersonalPreview";
+
 export default function Preview() {
-  return <div className="preview"></div>;
+  return (
+    <section className="preview" aria-label="cv preview">
+      <PersonalPreview />
+      <EducationPreview />
+      <ExperiencePreview />
+    </section>
+  );
 }
