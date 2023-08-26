e8894c72f24ccd21040dcb6fc90ba60e069f224b
deviant <mb.deviant@gmail.com>
2023-08-26 23:58:14 +0300

    add initial states to update preview on user input

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Education from "./CVForm/Education";
 import Experience from "./CVForm/Experience";
 import PersonalInfo from "./CVForm/PersonalInfo";
+import Preview from "./CVPreview/Preview";
 
 export default function Content() {
+  const [name, setName] = useState(null);
+
+  function onName(e) {
+    setName(e.target.value);
+  }
+
   return (
     <>
       <div className="content">
-        <div className="forms">
-          <PersonalInfo />
-          <Education />
-          <Experience />
+        <div className="mainContainer">
+          <div className="forms">
+            <PersonalInfo onName={onName} />
+            <Education />
+            <Experience />
+          </div>
+          <Preview name={name} />
         </div>
       </div>
     </>
