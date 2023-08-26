e6cbfacfd9db7925999ada436a2816fa23fbb8e0
deviant <mb.deviant@gmail.com>
2023-08-26 21:53:01 +0300

    create initial personal preview component

---
+++ src/components/CVPreview/PersonalPreview.jsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faLocationDot,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function PersonalPreview({
+  name,
+  surname,
+  adress,
+  phone,
+  email,
+  about,
+}) {
+  return (
+    <div className="personalPreview">
+      <h3>
+        {name} {surname}
+      </h3>
+      <p>
+        <FontAwesomeIcon icon={faEnvelope} />
+        {email}
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faLocationDot} />
+        {adress}
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faPhone} />
+        {phone}
+      </p>
+      <p>{about}</p>
+    </div>
+  );
+}
