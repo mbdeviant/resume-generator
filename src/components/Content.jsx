5559dcd4a322a33628f3e0f8c40e4633de9268ee
deviant <mb.deviant@gmail.com>
2023-08-27 04:13:59 +0300

    add functions to handle other form inputs and pass them to preview as props

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,6 +7,10 @@ import Preview from "./CVPreview/Preview";
 export default function Content() {
   const [name, setName] = useState(null);
   const [surname, setSurname] = useState(null);
+  const [adress, setAdress] = useState(null);
+  const [number, setNumber] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [about, setAbout] = useState(null);
 
   function onName(e) {
     setName(e.target.value);
@@ -14,17 +18,43 @@ export default function Content() {
   function onSurname(e) {
     setSurname(e.target.value);
   }
+  function onAdress(e) {
+    setAdress(e.target.value);
+  }
+  function onNumber(e) {
+    setNumber(e.target.value);
+  }
+  function onEmail(e) {
+    setEmail(e.target.value);
+  }
+  function onAbout(e) {
+    setAbout(e.target.value);
+  }
 
   return (
     <>
       <div className="content">
         <div className="mainContainer">
           <div className="forms">
-            <PersonalInfo onName={onName} onSurname={onSurname} />
+            <PersonalInfo
+              onName={onName}
+              onSurname={onSurname}
+              onAdress={onAdress}
+              onNumber={onNumber}
+              onEmail={onEmail}
+              onAbout={onAbout}
+            />
             <Education />
             <Experience />
           </div>
-          <Preview name={name} surname={surname} />
+          <Preview
+            name={name}
+            surname={surname}
+            adress={adress}
+            number={number}
+            email={email}
+            about={about}
+          />
         </div>
       </div>
     </>
