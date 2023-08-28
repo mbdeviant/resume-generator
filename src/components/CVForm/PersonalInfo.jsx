841a59a6833e4745bc098f6ee3f2ebcc62181518
deviant <mb.deviant@gmail.com>
2023-08-28 22:18:38 +0300

    store form values in object to pass form data to parent component

--- src/components/CVForm/PersonalInfo.jsx
+++ src/components/CVForm/PersonalInfo.jsx
@@ -1,28 +1,86 @@
-export default function PersonalInfo({
-  onName,
-  onSurname,
-  onAdress,
-  onNumber,
-  onEmail,
-  onAbout,
-}) {
+import { useState } from "react";
+
+export default function PersonalInfo({ onSubmit }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    surname: "",
+    address: "",
+    number: "",
+    email: "",
+    about: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
   return (
     <>
       <section className="personalInfo">
         <h2>Personal Info</h2>
-        <form className="infoForm">
+        <form className="infoForm" onSubmit={handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" onChange={onName} required></input>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInput}
+            required
+          />
           <label htmlFor="surname">Surname</label>
-          <input type="text" id="surname" onChange={onSurname} required />
-          <label htmlFor="adress">Adress</label>
-          <input type="text" id="adress" onChange={onAdress} required />
+          <input
+            type="text"
+            id="surname"
+            name="surname"
+            value={formData.surname}
+            onChange={handleInput}
+            required
+          />
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleInput}
+            required
+          />
           <label htmlFor="number">Phone Number</label>
-          <input type="tel" id="number" onChange={onNumber} required />
+          <input
+            type="tel"
+            id="number"
+            name="number"
+            value={formData.number}
+            onChange={handleInput}
+            required
+          />
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" onChange={onEmail} required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInput}
+            required
+          />
           <label htmlFor="about">About</label>
-          <input type="text" id="about" onChange={onAbout} />
+          <input
+            type="text"
+            id="about"
+            name="about"
+            value={formData.about}
+            onChange={handleInput}
+          />
           <button type="submit" aria-label="save button">
             Save
           </button>
