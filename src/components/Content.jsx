f92c2a8e3c68b21318528a72dc418e0c87363741
deviant <mb.deviant@gmail.com>
2023-08-28 22:19:48 +0300

    simplify content component functions

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,60 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Education from "./CVForm/Education";
 import Experience from "./CVForm/Experience";
 import PersonalInfo from "./CVForm/PersonalInfo";
 import Preview from "./CVPreview/Preview";
 
 export default function Content() {
-  const [name, setName] = useState(null);
-  const [surname, setSurname] = useState(null);
-  const [adress, setAdress] = useState(null);
-  const [number, setNumber] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [about, setAbout] = useState(null);
+  const [formData, setFormData] = useState({});
 
-  function onName(e) {
-    const value = e.target.value;
-    setName(value);
-  }
-  function onSurname(e) {
-    setSurname(e.target.value);
-  }
-  function onAdress(e) {
-    setAdress(e.target.value);
-  }
-  function onNumber(e) {
-    setNumber(e.target.value);
-  }
-  function onEmail(e) {
-    setEmail(e.target.value);
-  }
-  function onAbout(e) {
-    setAbout(e.target.value);
-  }
+  const handleFormSubmit = (data) => {
+    setFormData(data);
+  };
+
+  useEffect(() => {}, [formData]);
 
   return (
     <>
       <div className="content">
         <div className="mainContainer">
           <div className="forms">
-            <PersonalInfo
-              onName={onName}
-              onSurname={onSurname}
-              onAdress={onAdress}
-              onNumber={onNumber}
-              onEmail={onEmail}
-              onAbout={onAbout}
-            />
+            <PersonalInfo onSubmit={handleFormSubmit} />
             <Education />
             <Experience />
           </div>
           <Preview
-            name={name}
-            surname={surname}
-            adress={adress}
-            number={number}
-            email={email}
-            about={about}
+            name={formData.name}
+            surname={formData.surname}
+            adress={formData.adress}
+            number={formData.number}
+            email={formData.email}
+            about={formData.about}
           />
         </div>
       </div>
