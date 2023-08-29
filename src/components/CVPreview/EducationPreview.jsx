82b86b3004eb764c2af9fa2f2914aaed08bb970e
deviant <mb.deviant@gmail.com>
2023-08-29 19:57:35 +0300

    change education preview div to section and add aria label

--- src/components/CVPreview/EducationPreview.jsx
+++ src/components/CVPreview/EducationPreview.jsx
@@ -1,11 +1,11 @@
 export default function EducationPreview({ educationInfo }) {
   return (
-    <div className="educationPreview">
+    <section aria-label="Education Preview" className="educationPreview">
       <h3>Education</h3>
       <p>{educationInfo.university}</p>
       <p>{educationInfo.department}</p>
-      <p>{educationInfo.start}</p>
-      <p>{educationInfo.graduation}</p>
-    </div>
+      <p>From {educationInfo.startDate}</p>
+      <p>To {educationInfo.graduation}</p>
+    </section>
   );
 }
