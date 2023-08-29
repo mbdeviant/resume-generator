1caff688deee0ec15fd49dfc09de5c7a8d6edd01
deviant <mb.deviant@gmail.com>
2023-08-29 19:31:18 +0300

    attach education info props to component

--- src/components/CVPreview/Preview.jsx
+++ src/components/CVPreview/Preview.jsx
@@ -2,11 +2,11 @@ import EducationPreview from "./EducationPreview";
 import ExperiencePreview from "./ExperiencePreview";
 import PersonalPreview from "./PersonalPreview";
 
-export default function Preview({ personalInfo }) {
+export default function Preview({ personalInfo, educationInfo }) {
   return (
     <section className="preview" aria-label="cv preview">
       <PersonalPreview personalInfo={personalInfo} />
-      <EducationPreview />
+      <EducationPreview educationInfo={educationInfo} />
       <ExperiencePreview />
     </section>
   );
