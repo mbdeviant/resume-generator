63f2f86781a339cdf46602790f31724be3821652
deviant <mb.deviant@gmail.com>
2023-08-29 19:57:13 +0300

    add aria label to preview section

--- src/components/CVPreview/Preview.jsx
+++ src/components/CVPreview/Preview.jsx
@@ -4,7 +4,7 @@ import PersonalPreview from "./PersonalPreview";
 
 export default function Preview({ personalInfo, educationInfo }) {
   return (
-    <section className="preview" aria-label="cv preview">
+    <section className="preview" aria-label="CV Preview">
       <PersonalPreview personalInfo={personalInfo} />
       <EducationPreview educationInfo={educationInfo} />
       <ExperiencePreview />
