6d9b02ecda20d825fabc11334d539d231a9fe01e
deviant <mb.deviant@gmail.com>
2023-08-30 23:34:44 +0300

    pass props to education preview component for item removing management

--- src/components/CVPreview/Preview.jsx
+++ src/components/CVPreview/Preview.jsx
@@ -2,11 +2,18 @@ import EducationPreview from "./EducationPreview";
 import ExperiencePreview from "./ExperiencePreview";
 import PersonalPreview from "./PersonalPreview";
 
-export default function Preview({ personalInfo, educationInfo }) {
+export default function Preview({
+  personalInfo,
+  educationInfo,
+  onRemoveEducation,
+}) {
   return (
     <section className="preview" aria-label="CV Preview">
       <PersonalPreview personalInfo={personalInfo} />
-      <EducationPreview educationInfo={educationInfo} />
+      <EducationPreview
+        educationInfo={educationInfo}
+        onRemoveEducation={onRemoveEducation}
+      />
       <ExperiencePreview />
     </section>
   );
