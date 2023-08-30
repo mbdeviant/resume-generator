c2b7bb58392f8f295e735a404ec41a16c478fdae
deviant <mb.deviant@gmail.com>
2023-08-30 21:27:11 +0300

    create map function to render every incoming data

--- src/components/CVPreview/EducationPreview.jsx
+++ src/components/CVPreview/EducationPreview.jsx
@@ -2,10 +2,14 @@ export default function EducationPreview({ educationInfo }) {
   return (
     <section aria-label="Education Preview" className="educationPreview">
       <h3>Education</h3>
-      <p>{educationInfo.university}</p>
-      <p>{educationInfo.department}</p>
-      <p>From {educationInfo.startDate}</p>
-      <p>To {educationInfo.graduation}</p>
+      {educationInfo.map((education, index) => (
+        <div key={index} className="educationItem">
+          <p>{education.university}</p>
+          <p>{education.department}</p>
+          <p>From {education.startDate}</p>
+          <p>To {education.graduation}</p>
+        </div>
+      ))}
     </section>
   );
 }
