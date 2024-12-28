d6b196030cecaff918b6d4de04884bf1e25a116d
deviant <mb.deviant@gmail.com>
2024-12-28 18:05:54 +0300

    relocate personal info elements

--- src/components/CVPreview/PersonalPreview.jsx
+++ src/components/CVPreview/PersonalPreview.jsx
@@ -16,16 +16,16 @@ export default function PersonalPreview({ personalInfo }) {
         <h3 className="title">{personalInfo.title}</h3>
       </div>
       <p className="infoIcon">
-        <FontAwesomeIcon icon={faLocationDot} />
-        {personalInfo.address}
+        <FontAwesomeIcon icon={faEnvelope} />
+        {personalInfo.email}
       </p>
       <p className="infoIcon">
         <FontAwesomeIcon icon={faPhone} />
         {personalInfo.number}
       </p>
       <p className="infoIcon">
-        <FontAwesomeIcon icon={faEnvelope} />
-        {personalInfo.email}
+        <FontAwesomeIcon icon={faLocationDot} />
+        {personalInfo.address}
       </p>
       <h4>Summary</h4>
       <p>{personalInfo.about}</p>
