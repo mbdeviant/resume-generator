4b87053ca531d74f546c01daca72acbae68c6845
deviant <mb.deviant@gmail.com>
2024-12-31 19:56:17 +0300

    remove data validation and allow users download the template

--- src/components/CVPreview/Preview.jsx
+++ src/components/CVPreview/Preview.jsx
@@ -19,33 +19,23 @@ export default function Preview({
       educationInfoPDF: educationInfo,
       experienceInfoPDF: experienceInfo,
     };
+
     setPDFData(data);
 
-    if (
-      pdfData &&
-      data.personalInfoPDF &&
-      data.educationInfoPDF &&
-      data.experienceInfoPDF
-    ) {
-      // Generate the PDF blob
-      const blob = await pdf(
-        <PDFPreview
-          personalInfo={data.personalInfoPDF}
-          educationInfo={data.educationInfoPDF}
-          experienceInfo={data.experienceInfoPDF}
-        />
-      ).toBlob();
+    const blob = await pdf(
+      <PDFPreview
+        personalInfo={data.personalInfoPDF}
+        educationInfo={data.educationInfoPDF}
+        experienceInfo={data.experienceInfoPDF}
+      />
+    ).toBlob();
 
-      // Create a download link and trigger it
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "cv.pdf";
-      link.click();
-      URL.revokeObjectURL(url); // Clean up
-    } else {
-      setShowPDF(false);
-    }
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "resume.pdf";
+    link.click();
+    URL.revokeObjectURL(url);
   };
   return (
     <div>
