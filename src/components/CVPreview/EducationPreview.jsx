26f4bf53cd87925647277870fa951a0d7663e70e
deviant <mb.deviant@gmail.com>
2025-03-20 11:44:50 +0300

    add dev note to education preview

--- src/components/CVPreview/EducationPreview.jsx
+++ src/components/CVPreview/EducationPreview.jsx
@@ -3,10 +3,10 @@ import "./preview.css";
 export default function EducationPreview({ educationInfo }) {
   return (
     <section aria-label="Education Preview" className="educationPreview">
-      <h4 aria-label="Education Preview Section">Education</h4>
       {educationInfo.length > 0 ? (
         educationInfo.map((education) => (
           <div key={education.id} className="educationItem">
+            <h4 aria-label="Education Preview Section">Education</h4>
             <div>
               <p className="bold" aria-label="University Name">
                 {education.university}
@@ -22,6 +22,12 @@ export default function EducationPreview({ educationInfo }) {
         ))
       ) : (
         <div className="educationItem">
+          <p aria-label="Developer's Note" style={{ color: "orange" }}>
+            This is just a preview. Only submitted data will appear in the PDF.
+            If no data is submitted in a section, that section will not appear
+            in the PDF.
+          </p>
+          <h4 aria-label="Education Preview Section">Education</h4>
           <p className="bold" aria-label="University Name">
             Fire Academy of Excellence
           </p>
